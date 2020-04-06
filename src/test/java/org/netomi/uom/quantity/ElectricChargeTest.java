44d3130944ca1c36aa68a04003d01a30f8c0591e
Thomas Neidhart <thomas.neidhart@gmail.com>
2020-04-06 16:19:23 +0200

    Add missing tests for quantities.

---
+++ src/test/java/org/netomi/uom/quantity/ElectricChargeTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020 Thomas Neidhart
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.netomi.uom.quantity;
+
+import org.netomi.uom.Unit;
+import org.netomi.uom.unit.Units;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * Unit test for the {@link ElectricCharge} quantity.
+ */
+public class ElectricChargeTest extends GenericQuantityTest<ElectricCharge, ElectricCharge> {
+
+    @Override
+    protected Class<ElectricCharge> getQuantityClass() {
+        return ElectricCharge.class;
+    }
+
+    @Override
+    protected Unit<ElectricCharge> getSystemUnit() {
+        return Units.SI.COULOMB;
+    }
+
+    @Override
+    protected BiFunction<Double, Unit<ElectricCharge>, ElectricCharge> getFactoryMethod() {
+        return ElectricCharge::of;
+    }
+
+    @Override
+    protected Function<Double, ElectricCharge> getFactoryMethodForSystemUnit() {
+        return ElectricCharge::ofCoulomb;
+    }
+}
