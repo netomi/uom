998c07cd3fb4c25be17e129088dd90517d92980f
Thomas Neidhart <thomas.neidhart@gmail.com>
2020-04-08 23:27:48 +0200

    javadoc update.

--- src/main/java/org/netomi/uom/QuantityFactory.java
+++ src/main/java/org/netomi/uom/QuantityFactory.java
@@ -19,7 +19,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 
 /**
- * A factory interface to be able to create quantities of a specified quantity type.
+ * A factory interface to be able to create quantities of a specific quantity type.
  *
  * @param <Q> the quantity type
  *
