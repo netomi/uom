96ed4ea1abc459bae8de4c1a0d51d799192402c3
Thomas Neidhart <thomas.neidhart@gmail.com>
2020-04-22 23:19:31 +0200

    Minor doc formatting, remove not needed class as we target Java 8+.

--- src/main/java/org/netomi/uom/util/ConcurrentReferenceHashMap.java
+++ src/main/java/org/netomi/uom/util/ConcurrentReferenceHashMap.java
@@ -133,14 +133,14 @@ import java.util.concurrent.locks.ReentrantLock;
  * @param <K> the type of keys maintained by this map
  * @param <V> the type of mapped values
  */
-final public class ConcurrentReferenceHashMap<K, V> extends AbstractMap<K, V>
+public final class ConcurrentReferenceHashMap<K, V> extends AbstractMap<K, V>
 		implements java.util.concurrent.ConcurrentMap<K, V>, Serializable {
 	private static final long serialVersionUID = 7249069246763182397L;
 
 	/*
-		 * The basic strategy is to subdivide the table among Segments,
-		 * each of which itself is a concurrently readable hash table.
-		 */
+	 * The basic strategy is to subdivide the table among Segments,
+	 * each of which itself is a concurrently readable hash table.
+	 */
 
 	/**
 	 * An option specifying which Java reference type should be used to refer
@@ -437,42 +437,41 @@ final public class ConcurrentReferenceHashMap<K, V> extends AbstractMap<K, V>
 	 */
 	static final class Segment<K,V> extends ReentrantLock implements Serializable {
 		/*
-				 * Segments maintain a table of entry lists that are ALWAYS
-				 * kept in a consistent state, so can be read without locking.
-				 * Next fields of nodes are immutable (final).  All list
-				 * additions are performed at the front of each bin. This
-				 * makes it easy to check changes, and also fast to traverse.
-				 * When nodes would otherwise be changed, new nodes are
-				 * created to replace them. This works well for hash tables
-				 * since the bin lists tend to be short. (The average length
-				 * is less than two for the default load factor threshold.)
-				 *
-				 * Read operations can thus proceed without locking, but rely
-				 * on selected uses of volatiles to ensure that completed
-				 * write operations performed by other threads are
-				 * noticed. For most purposes, the "count" field, tracking the
-				 * number of elements, serves as that volatile variable
-				 * ensuring visibility.  This is convenient because this field
-				 * needs to be read in many read operations anyway:
-				 *
-				 *   - All (unsynchronized) read operations must first read the
-				 *     "count" field, and should not look at table entries if
-				 *     it is 0.
-				 *
-				 *   - All (synchronized) write operations should write to
-				 *     the "count" field after structurally changing any bin.
-				 *     The operations must not take any action that could even
-				 *     momentarily cause a concurrent read operation to see
-				 *     inconsistent data. This is made easier by the nature of
-				 *     the read operations in Map. For example, no operation
-				 *     can reveal that the table has grown but the threshold
-				 *     has not yet been updated, so there are no atomicity
-				 *     requirements for this with respect to reads.
-				 *
-				 * As a guide, all critical volatile reads and writes to the
-				 * count field are marked in code comments.
-				 */
-
+		 * Segments maintain a table of entry lists that are ALWAYS
+		 * kept in a consistent state, so can be read without locking.
+		 * Next fields of nodes are immutable (final).  All list
+		 * additions are performed at the front of each bin. This
+		 * makes it easy to check changes, and also fast to traverse.
+		 * When nodes would otherwise be changed, new nodes are
+		 * created to replace them. This works well for hash tables
+		 * since the bin lists tend to be short. (The average length
+		 * is less than two for the default load factor threshold.)
+		 *
+		 * Read operations can thus proceed without locking, but rely
+		 * on selected uses of volatiles to ensure that completed
+		 * write operations performed by other threads are
+		 * noticed. For most purposes, the "count" field, tracking the
+		 * number of elements, serves as that volatile variable
+		 * ensuring visibility.  This is convenient because this field
+		 * needs to be read in many read operations anyway:
+		 *
+		 *   - All (unsynchronized) read operations must first read the
+		 *     "count" field, and should not look at table entries if
+		 *     it is 0.
+		 *
+		 *   - All (synchronized) write operations should write to
+		 *     the "count" field after structurally changing any bin.
+		 *     The operations must not take any action that could even
+		 *     momentarily cause a concurrent read operation to see
+		 *     inconsistent data. This is made easier by the nature of
+		 *     the read operations in Map. For example, no operation
+		 *     can reveal that the table has grown but the threshold
+		 *     has not yet been updated, so there are no atomicity
+		 *     requirements for this with respect to reads.
+		 *
+		 * As a guide, all critical volatile reads and writes to the
+		 * count field are marked in code comments.
+		 */
 		private static final long serialVersionUID = 2249069246763182397L;
 
 		/**
@@ -714,19 +713,18 @@ final public class ConcurrentReferenceHashMap<K, V> extends AbstractMap<K, V>
 				return 0;
 
 			/*
-						 * Reclassify nodes in each list to new Map.  Because we are
-						 * using power-of-two expansion, the elements from each bin
-						 * must either stay at same index, or move with a power of two
-						 * offset. We eliminate unnecessary node creation by catching
-						 * cases where old nodes can be reused because their next
-						 * fields won't change. Statistically, at the default
-						 * threshold, only about one-sixth of them need cloning when
-						 * a table doubles. The nodes they replace will be garbage
-						 * collectable as soon as they are no longer referenced by any
-						 * reader thread that may be in the midst of traversing table
-						 * right now.
-						 */
-
+			 * Reclassify nodes in each list to new Map.  Because we are
+			 * using power-of-two expansion, the elements from each bin
+			 * must either stay at same index, or move with a power of two
+			 * offset. We eliminate unnecessary node creation by catching
+			 * cases where old nodes can be reused because their next
+			 * fields won't change. Statistically, at the default
+			 * threshold, only about one-sixth of them need cloning when
+			 * a table doubles. The nodes they replace will be garbage
+			 * collectable as soon as they are no longer referenced by any
+			 * reader thread that may be in the midst of traversing table
+			 * right now.
+			 */
 			HashEntry<K,V>[] newTable = HashEntry.newArray(oldCapacity<<1);
 			threshold = (int)(newTable.length * loadFactor);
 			int sizeMask = newTable.length - 1;
@@ -1515,68 +1513,6 @@ final public class ConcurrentReferenceHashMap<K, V> extends AbstractMap<K, V>
 		@Override
 		public V nextElement() { return super.nextEntry().value(); }
 	}
-
-	/*
-		  * This class is needed for JDK5 compatibility.
-		  */
-	static class SimpleEntry<K, V> implements Entry<K, V>,
-			java.io.Serializable {
-		private static final long serialVersionUID = -8499721149061103585L;
-
-		private final K key;
-		private V value;
-
-		public SimpleEntry(K key, V value) {
-			this.key = key;
-			this.value = value;
-		}
-
-		public SimpleEntry(Entry<? extends K, ? extends V> entry) {
-			this.key = entry.getKey();
-			this.value = entry.getValue();
-		}
-
-		@Override
-		public K getKey() {
-			return key;
-		}
-
-		@Override
-		public V getValue() {
-			return value;
-		}
-
-		@Override
-		public V setValue(V value) {
-			V oldValue = this.value;
-			this.value = value;
-			return oldValue;
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			if (!(o instanceof Map.Entry))
-				return false;
-			Map.Entry<?,?> e = (Map.Entry<?,?>) o;
-			return eq(key, e.getKey()) && eq(value, e.getValue());
-		}
-
-		@Override
-		public int hashCode() {
-			return (key == null ? 0 : key.hashCode())
-					^ (value == null ? 0 : value.hashCode());
-		}
-
-		@Override
-		public String toString() {
-			return key + "=" + value;
-		}
-
-		private static boolean eq(Object o1, Object o2) {
-			return o1 == null ? o2 == null : o1.equals(o2);
-		}
-	}
-
 
 	/**
 	 * Custom Entry class used by EntryIterator.next(), that relays setValue
